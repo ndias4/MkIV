49e5cb70d5c55137c57041cd0d61beeb3b2de881
ndias4 <fdn.dias10@gmail.com>
2024-03-26 21:28:16 -0300

    first commit

---
+++ CalculadoraTeste.java
@@ -0,0 +1,11 @@
+public class CalculadoraTeste {
+     public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        //testar soma
+        double soma = calc.somar(2, 3);
+        System.out.println(soma);
+        //testar subtracao
+        double sub = calc.subtrair(2, 3);
+        System.out.println(sub);
+     }
+}
